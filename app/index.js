1ac1ec2dd5ac32e45f2647e620cebf41a9a18414
Kate Leslie <thisizk8@gmail.com>
2017-02-21 15:02:04 -0600

    Project complete

--- app/index.js
+++ app/index.js
@@ -1,14 +1,21 @@
 import 'whatwg-fetch';
+
 import fillInResult from './fill-in-result';
 
 const searchBar = document.querySelector('.search-bar');
 const searchButton = document.querySelector('.search-button');
 const results = document.querySelector('.main');
 
-searchButton.addEventListener('click', () => {
-  fetch(`http://netflixroulette.net/api/api.php?title=${searchBar.value}`)
+function search(term) {
+  fetch(`http://netflixroulette.net/api/api.php?title=${term}`)
     .then(response => response.json())
     .then((data) => {
       fillInResult(results, data);
     });
+}
+
+searchButton.addEventListener('click', () => {
+  search(searchBar.value);
 });
+
+search('Attack on Titan');

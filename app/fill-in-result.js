ea990bdbe5433761ab6f2dc6c9a0a968e2587301
Kate Leslie <thisizk8@gmail.com>
2017-02-20 21:10:29 -0600

    added some js

---
+++ app/fill-in-result.js
@@ -0,0 +1,4 @@
+export default function (element, data) {
+  element.querySelector('.show_title').innerHTML = data.show_title;
+  console.log(data);
+}
--- app/index.js
+++ app/index.js
@@ -1,3 +1,14 @@
 import 'whatwg-fetch';
+import fillInResult from './fill-in-result';
+
 const searchBar = document.querySelector('.search-bar');
 const searchButton = document.querySelector('.search-button');
+const results = document.querySelector('.main');
+
+searchButton.addEventListener('click', () => {
+  fetch(`http://netflixroulette.net/api/api.php?title=${searchBar.value}`)
+    .then(response => response.json())
+    .then((data) => {
+      fillInResult(results, data);
+    });
+});
